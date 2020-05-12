e4b06c08e3935c7a7a6f637005cb485910cd93e6
Kwankyu <kwankyu.kko@gmail.com>
2020-05-11 22:10:09 -0600

    done, you?

--- 20-validParaetheses/kwan.py
+++ 20-validParaetheses/kwan.py
@@ -2,3 +2,28 @@
  * @param {string} s
  * @return {boolean}
  */
+var isValid = function(s) {
+    if (s.iength === 0) return true;
+    
+    const comp = {
+        "{": "}",
+        "(": ")",
+        "[": "]",
+    };
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '{' || s[i] === '(' || s[i] === '[' ) {
+            stack.push(s[i])
+        }
+        else if (s[i] === '}' || s[i] === ')' || s[i] === ']' ) {
+            if (comp[stack[stack.length-1]] === s[i]) {
+                stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    if (stack.length === 0) return true;
+    return false;
+};
