b5ea5f5b4bbec17e5e16cec3025e6d4ff436ff1d
yaejinlee93 <yaejinlee93@gmail.com>
2020-05-14 05:09:37 -0600

    shorter answer

--- 26-removeDuplicatesFromSortedArray/yaejin.py
+++ 26-removeDuplicatesFromSortedArray/yaejin.py
@@ -4,21 +4,15 @@ def removeDuplicates(nums):
     :rtype: int
     """
     length = 1
-    index = 1
 
     if len(nums) == 0:
         return 0
     
     for i in range(1, len(nums)):
         if nums[i] != nums[i - 1]:
+            nums[length] = nums[i]             
             length += 1
-            nums[index] = nums[i]             
-            index += 1
 
-    print(length)
     return length
-
-
-    
 
 removeDuplicates([0,0,1,1,1,2,2,3,3,4])
